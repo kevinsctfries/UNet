b207704b4348c5b3ca7c1671df40c020ceb4781b
Kevin Fries <kevinsctfries@gmail.com>
2025-02-08 01:34:11 -0500

    Add sidebar stylings

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,9 +1,9 @@
 const Sidebar = () => {
   return (
-    <div className="bg-blue-100 h-full text-gray-800 p-4 space-y-8 shadow-lg">
+    <div className="bg-blue-100 h-full text-gray-800 p-4 space-y-8 shadow-md">
       <div>
         <span className="text-lg font-semibold text-blue-700">Unions</span>
-        <ul className="mt-4 space-y-2">
+        <ul className="mt-4 p-4 space-y-2 bg-white rounded-lg shadow-sm">
           <li className="hover:text-blue-400 cursor-pointer transition-colors duration-300">
             Link 1
           </li>
@@ -23,7 +23,7 @@ const Sidebar = () => {
       </div>
       <div>
         <span className="text-lg font-semibold text-blue-700">Resources</span>
-        <ul className="mt-4 space-y-2">
+        <ul className="mt-4 p-4 space-y-2 bg-white rounded-lg shadow-sm">
           <li className="hover:text-blue-400 cursor-pointer transition-colors duration-300">
             Link 1
           </li>
